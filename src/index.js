78c2b98e172447eeecc6552b217b2613cdf423c6
FaezAnsar <FAEZANSARAHMED2004@GMAIL.COM>
2025-02-04 10:52:11 +0500

    mongodb setup and connected

---
+++ src/index.js
@@ -0,0 +1,25 @@
+
+import 'dotenv/config'
+
+import connectDB from './db/index.js';
+
+connectDB ()
+// import express from "express"
+// const app = express()
+// (async()=>{
+// try{
+//    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//    app.on("error",(error)=>{
+//     console.log("err:",error);
+//     throw error
+//    })
+//    app.listen(process.env.PORT,()=>{
+//     console.log(    `App is listening on port ${process.env.PORT}`);
+//    })
+// }catch(error){
+//     console.error("error:",error);
+//     throw error
+    
+// }
+
+// })()
