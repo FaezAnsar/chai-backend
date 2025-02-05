191c36070c6d7aab78aab5f493b5941ec68c8856
FaezAnsar <FAEZANSARAHMED2004@GMAIL.COM>
2025-02-05 10:31:51 +0500

    utils

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@
 import 'dotenv/config'
 
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
-connectDB ()
+connectDB ().then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(  `server is running at port : ${process.env.PORT}`);
+    })
+}).catch((err)=>{
+    console.log("Mongodb connection failed",err)
+})
 // import express from "express"
 // const app = express()
 // (async()=>{
